a295d248b6702ee48396538d24ec0d81b9548398
Jae Sun Lee <jaesun90@gmail.com>
2020-03-24 15:05:21 +0900

    Update Crawler.py

--- Crawler.py
+++ Crawler.py
@@ -13,7 +13,7 @@ import os
 import socket
 import sys
 
-baseurl = "https://www.envylook.com"
+base_url = "https://www.envylook.com"
 
 envydata = []
 
@@ -70,24 +70,24 @@ def product_detail(no, url):
         temp.append("")
         print(i)
 
-    imgfilecount = 1
+    imgfile_count = 1
     for d in images:
         s = d.get("src")
         if '//' in s:
             temp.append(s)
-            save_img(str(no) + "-" + str(imgfilecount), 'http:' + s)
+            save_img(str(no) + "-" + str(imgfile_count), 'http:' + s)
             print('http:' + s, end="\n")
         else:
-            temp.append(baseurl + s)
-            save_img(str(no) + "-" + str(imgfilecount), baseurl + s)
-            print(baseurl + s, end="\n")
-        imgfilecount += 1
+            temp.append(base_url + s)
+            save_img(str(no) + "-" + str(imgfile_count), base_url + s)
+            print(base_url + s, end="\n")
+        imgfile_count += 1
     envydata.append(temp)
 
 
 def envylook_category(category, page):  # Local variable name hid the variable defined in the outer space.
-    tailurl = f"/product/list2.html?cate_no={category}&page={page}"
-    url = baseurl + tailurl
+    tail_url = f"/product/list2.html?cate_no={category}&page={page}"
+    url = base_url + tail_url
 
     '''
     urllib not only opens http:// or https:// URLs, but also ftp:// and file://. 
@@ -96,11 +96,10 @@ def envylook_category(category, page):  # Local variable name hid the variable d
     '''
 
     if url.lower().startswith('http'):
-        req = urllib.Request.request(url)
+        html = urlopen(url).read()
     else:
         raise ValueError from None
 
-    html = urlopen(url).read()
     soup = BeautifulSoup(html, 'html.parser')
     info = soup.select(".thumbnail a")
     info2 = soup.find('meta', {'property': 'og:description'})
@@ -112,7 +111,7 @@ def envylook_category(category, page):  # Local variable name hid the variable d
 
     n = 1
     for d in info:
-        s = baseurl + d.get("href")
+        s = base_url + d.get("href")
         os.mkdir(str(n))
         os.chdir(str(n))
         product_detail(n, s)
@@ -128,15 +127,17 @@ page_no = input("페이지번호를 입력하세요 : ")
 socket.setdefaulttimeout(30)
 result = envylook_category(cate_no, page_no)
 
-csvname = result + ".csv"
+csv_name = result + ".csv"
 
-with open(csvname, 'w', newline='') as f:
+with open(csv_name, 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(
-        ['폴더번호', '주소', '상품명', '브랜드', '종류', '타이틀이미지', '정상가', '통화', '할인가', '통화', '요약설명', '옵션1', '옵션2', '옵션3', '옵션4',
-         '옵션5', '옵션6', '옵션7', '옵션8', '옵션9', '옵션10', '옵션11', '옵션12', '옵션13', '옵션14', '옵션15', '옵션16', '옵션17', '옵션18',
-         '옵션19', '옵션20', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지',
-         '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지'])
+        ['폴더번호', '주소', '상품명', '브랜드', '종류', '타이틀이미지', '정상가', '통화', '할인가', '통화', '요약설명',
+         '옵션1', '옵션2', '옵션3', '옵션4', '옵션5', '옵션6', '옵션7', '옵션8', '옵션9', '옵션10', '옵션11', '옵션12',
+         '옵션13', '옵션14', '옵션15', '옵션16', '옵션17', '옵션18', '옵션19', '옵션20',
+         '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지',
+         '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지', '상세이미지'
+         ])
     writer.writerows(envydata)
 
 # for d in info:
